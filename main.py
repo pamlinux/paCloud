19d272534c4112ba2ff0d1b905d5cb2066fc9387
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-16 22:46:36 +0200

    Query parameter type conversion.

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ app = FastAPI()
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: str, q: Optional[str] = None):
+async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
     if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
