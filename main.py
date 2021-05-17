2f7dde98596b981de3be8bd8a2748d11d71a2210
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-17 16:24:31 +0200

    Query parameter list / multiple values.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+from typing import List, Optional
+
 from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: str = Query(..., min_length=3)):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
+async def read_items(q: Optional[List[str]] = Query(None)):
+    query_items = {"q": q}
+    return query_items
