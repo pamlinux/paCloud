c0dff15a2910685ede59e0b3e7893eca9b1260d6
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-17 10:59:26 +0200

    Required query parameters.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from typing import Optional
 
 from fastapi import FastAPI
@@ -6,15 +5,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/users/{user_id}/items/{item_id}")
+@app.get("/items/{item_id}")
 async def read_user_item(
-    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+    item_id: str, needy: str, skip: int = 0, limit: Optional[int] = None
 ):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
     return item
