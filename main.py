3c460e07b26b079d3eadb95a680fdfffcae8059c
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-17 22:15:08 +0200

    Multiple body parameters.

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from fastapi import FastAPI, Path
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_items(
-    *, item_id: int = Path(..., title="The ID of the item to get", ge=1), q: str
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
+class User(BaseModel):
+    username: str
+    full_name: Optional[str] = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, user: User):
+    results = {"item_id": item_id, "item": item, "user": user}
     return results
