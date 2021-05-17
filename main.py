79d98c812b2c595df0532d02eb5284af096bcd92
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-17 14:32:44 +0200

    Request body + path + query parameters.

--- main.py
+++ main.py
@@ -14,12 +14,10 @@ class Item(BaseModel):
 app = FastAPI()
 
 
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
-
-
+@app.post("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: Optional[str] = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    result.update({"total_price" : item.price + item.tax})
+    return result
