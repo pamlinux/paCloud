50c6f34be8222a7c7367d9a512c921e955a9e231
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-17 15:49:06 +0200

    Additional validation : enforce q length doesn't exceed 50 characters.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(q: Optional[str] = None):
+async def read_items(q: Optional[str] = Query(None, max_length=50)):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
