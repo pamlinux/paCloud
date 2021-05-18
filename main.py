a909b815499a02f376276e5f041a70df81cc0c45
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-18 08:13:28 +0200

    Embed a single body parameter.

--- main.py
+++ main.py
@@ -13,21 +13,7 @@ class Item(BaseModel):
     tax: Optional[float] = None
 
 
-class User(BaseModel):
-    username: str
-    full_name: Optional[str] = None
-
-
 @app.put("/items/{item_id}")
-async def update_item(
-    *,
-    item_id: int,
-    item: Item,
-    user: User,
-    importance: int = Body(..., gt=0),
-    q: Optional[str] = None
-):
-    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
-    if q:
-        results.update({"q": q})
+async def update_item(item_id: int, item: Item = Body(..., embed=True)):
+    results = {"item_id": item_id, "item": item}
     return results
