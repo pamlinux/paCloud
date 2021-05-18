a6de1f12ff59cb2ec3023602a24a0a88fd137338
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-18 19:56:29 +0200

    Bodies of arbitrary dicts.

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
 """
+Bodies of arbitrary dicts with dict :
 
-with body list example :
-
-[
-    {
-        "url" : "https://www.rts.ch/info/12200906-le-canton-de-vaud-ouvre-la-vaccination-aux-jeunes-des-16-ans.html",
-        "name" : "RTS"
-    },
-    {
-        "url" : "https://www.rts.ch/info/12200906-le-canton-de-vaud-ouvre-la-vaccination-aux-jeunes-des-16-ans.html",
-        "name" : "RTS2"
-    }
-]
-
+{
+    "1" : 1,
+    "2" : 2,
+    "3" : 3
+}
 """
-from typing import List
+
+from typing import Dict
 
 from fastapi import FastAPI
-from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
 
 
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
-
-
-@app.post("/images/multiple/")
-async def create_multiple_images(images: List[Image]):
-    for image in images:
-        print(image.url)
-    return images
+@app.post("/index-weights/")
+async def create_index_weights(weights: Dict[int, float]):
+    for key in weights:
+        print(type(key))
+    return weights
