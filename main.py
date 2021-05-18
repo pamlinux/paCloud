062ae69ddbd5d642b87d4f453252826e720d6a24
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-18 23:28:50 +0200

    Declare Request Example Data : Field additional arguments.

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
 from typing import Optional
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
 class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "name": "Foo",
-                "description": "A very nice Item",
-                "price": 35.4,
-                "tax": 3.2,
-            }
-        }
+    name: str = Field(..., example="Foo")
+    description: Optional[str] = Field(None, example="A very nice Item")
+    price: float = Field(..., example=35.4)
+    tax: Optional[float] = Field(None, example=3.2)
 
 
 @app.put("/items/{item_id}")
