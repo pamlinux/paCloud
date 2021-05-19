8664b480d789db8282f25b3b21b84fc67fecb985
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-19 23:42:11 +0200

    Header Parameters.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from typing import Optional
 
-from fastapi import Cookie, FastAPI
+from fastapi import FastAPI, Header
 
 app = FastAPI()
 
 
 @app.get("/items/")
-async def read_items(ads_id: Optional[str] = Cookie(None)):
-    return {"ads_id": ads_id}
+async def read_items(user_agent: Optional[str] = Header(None)):
+    return {"User-Agent": user_agent}
