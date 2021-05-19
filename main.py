2b04c7f6614c99bc662768624ffcf001c9ce1211
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-19 21:30:46 +0200

    Other data types.

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
+from datetime import datetime, time, timedelta
+
 from typing import Optional
 
+from uuid import UUID
+
+
 from fastapi import Body, FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-
-
 @app.put("/items/{item_id}")
-async def update_item(
-    item_id: int,
-    item: Item = Body(
-        ...,
-        example={
-            "name": "Foo",
-            "description": "A very nice Item",
-            "price": 35.4,
-            "tax": 3.2,
-        },
-    ),
+async def read_items(
+
+    item_id: UUID,
+
+    start_datetime: Optional[datetime] = Body(None),
+
+    end_datetime: Optional[datetime] = Body(None),
+
+    repeat_at: Optional[time] = Body(None),
+
+    process_after: Optional[timedelta] = Body(None),
+
 ):
-    results = {"item_id": item_id, "item": item}
-    return results
+    start_process = start_datetime + process_after
+    duration = end_datetime - start_process
+    return {
+        "item_id": item_id,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime,
+        "repeat_at": repeat_at,
+        "process_after": process_after,
+        "start_process": start_process,
+        "duration": duration,
+    }
+
+
