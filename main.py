bb1040d41a64347c53299ca2e611098bf1df40c8
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-20 19:40:41 +0200

    Request Files

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
 
-@app.post("/login/")
-async def login(username: str = Form(...), password: str = Form(...)):
-    return {"username": username}
+@app.post("/files/")
+async def create_file(file: bytes = File(...)):
+    print(type(file))
+    return {"file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    print(type(file))
+    print(dir(file))
+    return {"filename": file.filename}
