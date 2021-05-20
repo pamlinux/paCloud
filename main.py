ac472b512d36086960b5da78ebc0817a8e1a1ecb
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-20 09:03:15 +0200

    Response Model.

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from typing import Optional
+from typing import List, Optional
 
-from fastapi import FastAPI, Header
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/items/")
-async def read_items(user_agent: Optional[str] = Header(None)):
-    return {"User-Agent": user_agent}
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+    tags: List[str] = []
+
+
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item):
+    return item
