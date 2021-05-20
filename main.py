7fff6109c254692fd1471d2b1573921a58ca0ba4
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-20 13:38:44 +0200

    Response Status Code.

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from typing import Dict
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/keyword-weights/", response_model=Dict[str, float])
-async def read_keyword_weights():
-    return {"foo": 2.3, "bar": 3.4}
-
+@app.post("/items/", status_code=201)
+async def create_item(name: str):
+    return {"name": name}
