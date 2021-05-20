1b546ffc1313fa39629b216b5736e64206381b72
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-20 13:07:25 +0200

    Union or anyOf.

--- main.py
+++ main.py
@@ -1,41 +1,35 @@
-from typing import Optional
+from typing import Union
 
 from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-class UserBase(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: Optional[str] = None
+class BaseItem(BaseModel):
+    description: str
+    type: str
 
 
-class UserIn(UserBase):
-    password: str
+class CarItem(BaseItem):
+    type = "car"
 
 
-class UserOut(UserBase):
-    pass
+class PlaneItem(BaseItem):
+    type = "plane"
+    size: int
 
 
-class UserInDB(UserBase):
-    hashed_password: str
+items = {
+    "item1": {"description": "All my friends drive a low rider", "type": "car"},
+    "item2": {
+        "description": "Music is my aeroplane, it's my aeroplane",
+        "type": "plane",
+        "size": 5,
+    },
+}
 
 
-def fake_password_hasher(raw_password: str):
-    return "supersecret" + raw_password
-
-
-def fake_save_user(user_in: UserIn):
-    hashed_password = fake_password_hasher(user_in.password)
-    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
-    print("User saved! ..not really", user_in_db)
-    return user_in_db
-
-
-@app.post("/user/", response_model=UserOut)
-async def create_user(user_in: UserIn):
-    user_saved = fake_save_user(user_in)
-    return user_saved
+@app.get("/items/{item_id}", response_model=Union[PlaneItem, CarItem])
+async def read_item(item_id: str):
+    return items[item_id]
