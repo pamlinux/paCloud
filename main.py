db53349b80178714340f027d8ee278bb575905fe
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2021-05-25 16:14:47 +0200

    Use StaticFiles.

--- main.py
+++ main.py
@@ -1,34 +1,6 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
-tags_metadata = [
-    {
-        "name": "users",
-        "description": "Operations with users. The **login** logic is also here.",
-    },
-    {
-        "name": "items",
-        "description": "Manage items. So _fancy_ they have their own docs.",
-        "externalDocs": {
-            "description": "Items external docs",
-            "url": "https://fastapi.tiangolo.com/",
-        },
-    },
-]
+app = FastAPI()
 
-app = FastAPI(
-    openapi_tags=tags_metadata,
-    title="My Super Project",
-    description="This is a very fancy project, with auto docs for the API and everything",
-    version="2.5.0",
-)
-
-
-
-@app.get("/users/", tags=["users"])
-async def get_users():
-    return [{"name": "Harry"}, {"name": "Ron"}]
-
-
-@app.get("/items/", tags=["items"])
-async def get_items():
-    return [{"name": "wand"}, {"name": "flying broom"}]
+app.mount("/static", StaticFiles(directory="static"), name="static")
